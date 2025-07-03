b41f300c5061450255e576425dcc7bfc45c8ebb6
Kumar Pranav <pranavthakur07@gmail.com>
2025-07-04 01:29:11 +0530

    prod deployment test2

--- src/main/java/com/chatapp/ChatAppApplication.java
+++ src/main/java/com/chatapp/ChatAppApplication.java
@@ -1,11 +1,26 @@
 package com.chatapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ChatAppApplication {
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void printActiveProfile() {
+        System.out.println(">>>>> Runtime active profile: " + Arrays.toString(env.getActiveProfiles()));
+    }
+
     public static void main(String[] args) {
+        System.out.println(">>>>> Runtime active args: " + Arrays.toString(args));
         SpringApplication.run(ChatAppApplication.class, args);
     }
 }
